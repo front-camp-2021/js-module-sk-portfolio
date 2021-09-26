19b851ee5e0cbfa81aff7bf65edf86eeea878304
Serhyi <catalonya1111@gmail.com>
2021-09-26 16:31:57 +0300

    refactor cutString solution

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -1,5 +1,8 @@
-export const cutStrings = (arr = []) => {
-  const lengthArr = arr.map(str => str.length)
-  const minLength = Math.min(...lengthArr)
-  return arr.map(str => str.substring(0, minLength))
+export const cutStrings = (arr = [], strLength = 0) => {
+  if(strLength <= 0) {
+    const lengthArr = arr.map(str => str.length)
+    strLength = Math.min(...lengthArr)
+  }
+  return arr.map(str => str.substring(0, strLength))
 }
+cutStrings(["adfg", "bkfkdl", "cdfffk", "dgldssvv"], 4)
